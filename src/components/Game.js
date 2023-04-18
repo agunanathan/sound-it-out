7c0f1e09421c859773ea5ebe8caf7802fa668583
agunanathan <asokan.gunanathan@gmail.com>
2023-04-18 14:00:59 -0400

    second commit

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,12 +2,31 @@ import React from 'react'
 
 export default function Game() {
     return (
-        <div className="game-wrapper">
-            <img src='nhl-toronto-maple-leafs.png' alt='logo'></img>
-            <h2> Toronto Maple Leafs vs Tampa Bay Lightning</h2>
-            <h3> Tuesday April 19th, 7:00 pm EST </h3>
-            <img src='nhl-tampa-bay-lightning.png' alt='logo'></img>
-
+        <div className='games-wrapper'>
+            <div className="game-container">
+                <div className = "team-info">
+                    <img className="team-image" src='nhl-toronto-maple-leafs.png' alt='logo'></img>
+                </div>
+                <div className="game-info">
+                    <h5> Toronto Maple Leafs @ Tampa Bay Lightning</h5>
+                    <h6>Tuesday April 19th, 7:00 pm EST </h6>
+                </div>
+                <div className="team-info">
+                    <img className="team-image" src='nhl-tampa-bay-lightning.png' alt='logo'></img>
+                </div>
+            </div>
+            <div className="game-container">
+                <div className = "team-info">
+                    <img className="team-image" src='nhl-boston-bruins.png' alt='logo'></img>
+                </div>
+                <div className="game-info">
+                    <h5> Boston Bruins @ Toronto Maple Leafs</h5>
+                    <h6>Tuesday April 21st, 7:00 pm EST </h6>
+                </div>
+                <div className="team-info">
+                    <img className="team-image" src='nhl-toronto-maple-leafs.png' alt='logo'></img>
+                </div>
+            </div>
         </div>
     );
 }
