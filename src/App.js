081781b13118cb1dddc9ed98622c0e0872b8a1f2
Karen Cardoz <karen.cardoz@rangle.io>
2023-05-01 13:14:52 -0400

    Resolved merge conflict

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import './App.css'
-<<<<<<< HEAD
+
 import Temporary from './components/Temporary'
-=======
-import Game from './components/Game'
-import Search from './components/Search'
-import Home from './components/Home'
->>>>>>> aac4d87 (simple login interface)
 
 function App() {
   return (
--- src/firebase_setup/firebase.js
+++ src/firebase_setup/firebase.js
@@ -4,15 +4,14 @@ import 'firebase/compat/firestore'
 import 'firebase/compat/database'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBuKf7knVtK1iZtdbl1izUggg9CWSwj5q0',
-  authDomain: 'test-5447f.firebaseapp.com',
-  projectId: 'test-5447f',
-  storageBucket: 'test-5447f.appspot.com',
-  messagingSenderId: '44311559632',
-  appId: '1:44311559632:web:2a520c5955c0cc70ee0dd5',
-  measurementId: 'G-NM53TWRFTB',
+  apiKey: 'AIzaSyCgboD9f6pdWnDHvPLHg_RLBLg4hfc0dAM',
+  authDomain: 'sounditout-7280c.firebaseapp.com',
+  projectId: 'sounditout-7280c',
+  storageBucket: 'sounditout-7280c.appspot.com',
+  messagingSenderId: '1087968572',
+  appId: '1:1087968572:web:8e3fdd7d93382c0172ba39',
   // The value of `databaseURL` depends on the location of the database
-  databaseURL: 'https://test-5447f-default-rtdb.firebaseio.com/',
+  databaseURL: 'https://sounditout-7280c-default-rtdb.firebaseio.com/',
 }
 
 // Initialize Firebase
