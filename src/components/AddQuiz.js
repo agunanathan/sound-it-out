0e3e9280e0ff0583a1cfb0ee2a5bad1813696e95
Karen Cardoz <karen.cardoz@rangle.io>
2023-05-03 10:55:37 -0400

    installed uid and font awesome and addedd a function to AddQuiz.js

--- src/components/AddQuiz.js
+++ src/components/AddQuiz.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Component } from 'react'
 import firebase from '../firebase_setup/firebase'
 import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
@@ -8,30 +8,29 @@ export default function AddQuiz() {
   // Declare multiple state variables!
   const [label, setLabel] = useState('Add a Quiz')
   const [button, setButton] = useState('Create')
-  const [message, setMessage] = useState(null)
+  const [message, setMessage] = useState('')
+  const [isConnected, setIsConnected] = useState(false)
+  const [database, setDatabase] = useState(null)
 
-  // useEffect(() => {
-  //   console.log(firebase.database)
-  // }, [])
-
-  // e is the event, which in this case is change, target is the element that triggered the event,
-  // which in this case is the input, and value is the value of the input element
-  // onChange = (e) => {
-  //   // console.log(e.target.id)
-  //   if (e.target.id === 'quizName') setMessage(e.useState.value)
-  // }
-
-  // addQuiz = (e) => {
-  //   const { message, button } = this.state
-  //   console.log({ message })
-  // }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(message)
+  }
 
   return (
     <div style={{ textAlign: 'center' }} className="container-content">
-      <label>{label}</label>
-      <span>: </span>
-      <input type="text" placeholder="Quiz Name" id="quizName"></input>
-      <button style={{ margin: '10px' }}>{button}</button>
+      <form className="AddQuizForm" onSubmit={handleSubmit}>
+        <label>{label}</label>
+        <span>: </span>
+        <input
+          type="text"
+          placeholder="Quiz Name"
+          id="quizName"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></input>
+        <button style={{ margin: '10px' }}>{button}</button>
+      </form>
     </div>
   )
 }
