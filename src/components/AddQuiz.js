5cd7fc3cbc4ac4c933aac3b39d5136f6af6e5374
Karen Cardoz <karen.cardoz@rangle.io>
2023-05-03 14:25:36 -0400

    using the set() to set additional quiz properties

--- src/components/AddQuiz.js
+++ src/components/AddQuiz.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify'
+import { v4 as uuid4 } from 'uuid'
 
 import firebase from '../firebase_setup/firebase'
 import 'firebase/compat/auth'
@@ -13,19 +14,33 @@ export default function AddQuiz({ addQuiz }) {
   const [button] = useState('Create')
   const [message, setMessage] = useState('')
   const dbRef = firebase.database().ref()
+  const newPostRef = dbRef.push()
 
   const handleSubmit = (e) => {
     // prevent default behavior of reloading forms
     e.preventDefault()
     if (!message) toast.error('Please enter a title')
     else {
-      dbRef.child('quiz').push(message, (err) => {
-        if (err) {
-          toast.error(err)
-        } else {
-          toast.success('Quiz Added Successfully')
-        }
-      })
+      dbRef
+        .child('quiz')
+        .push()
+        .set(
+          {
+            id: uuid4(),
+            title: message,
+            completed: false,
+            status: 'active',
+            isEditing: false,
+            countStudents: 0,
+          },
+          (err) => {
+            if (err) {
+              toast.error(err)
+            } else {
+              toast.success('Quiz Added Successfully')
+            }
+          }
+        )
     }
     addQuiz(message)
     setMessage('')
